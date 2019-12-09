5009ca822e62b40c4a4293ac2ca9870e65fdd242
Scott Grobe <Gitgrobe@gmail.com>
2019-12-08 22:55:40 -0500

    mvp

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+
+import React from 'react';
+import * as rtl from "@testing-library/react";
+
+import Dashboard from './Dashboard';
+
+test ('Dashboard renders correctly', () => {
+    expect(rtl.render(<Dashboard/>)).toMatchSnapshot();
+});
